5d5c22fe567962fd515561a23386bcf31d756b5b
toha <tohasvs@gmail.com>
2013-09-15 17:43:02 +0400

    add first project

---
+++ MavenGeo/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Антон
+ * Date: 15.09.13
+ * Time: 13:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
